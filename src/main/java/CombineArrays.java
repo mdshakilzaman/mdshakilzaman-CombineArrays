16495ba2ef3d4f94eec77a9870ca3b7e34994db9
Md Shakil Zaman <mdshakilzaman2020@gmail.com>
2023-03-05 02:28:29 +0000

    first commit

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -9,6 +9,9 @@ public class CombineArrays {
      */
     public int[] combine(int[] arr1, int[] arr2){
 
-        return null;
+        int[] combinedArray = new int[arr1.length + arr2.length];
+        System.arraycopy(arr1, 0, combinedArray, 0, arr1.length);
+        System.arraycopy(arr2, 0, combinedArray, arr1.length, arr2.length);
+        return combinedArray;
     }
 }
